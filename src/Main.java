9af609223b4f0825d455ae6e5a3d0f4398800a4b
Andrey Igumnov <igumnov92@gmail.com>
2022-09-24 14:10:43 +0300

    Добавление файлов

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static List<Person> generateClients() {
+        List<Person> clients = new LinkedList<>();
+        clients.add(new Person("Илья Мышкин", 2));
+        clients.add(new Person("Сергей Березин", 3));
+        clients.add(new Person("Руслан Савельев", 6));
+        clients.add(new Person("Артем Васин", 4));
+        clients.add(new Person("Олег Поцелуев", 5));
+        return clients;
+    }
+
+    public static void main(String[] args) {
+        Queue<Person> attraction = new LinkedList<>();
+        for (Person p : generateClients()) {
+            attraction.offer(p);
+        }
+
+        while (!attraction.isEmpty()) {
+            Person people = attraction.poll();
+            System.out.println(people.getName() + " прокатился на аттракционе!");
+            people.down();
+            if (people.getTickets() != 0) {
+                attraction.offer(people);
+            }
+        }
+    }
+}
